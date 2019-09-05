b764afdf411d5f1961d27deb37afcdba9884253e
TahmidRahman <tahmid131@gmail.com>
2019-09-05 19:55:28 +0600

    Add graph commonJS module

--- robot.js
+++ robot.js
@@ -1,3 +1,5 @@
+const { buildGraph } = require('./graph');
+
 const roads = [
   "Alice's House-Bob's House",
   "Alice's House-Cabin",
@@ -15,24 +17,7 @@ const roads = [
   'Shop-Town Hall'
 ];
 
-function buildGraph(edges) {
-  let graph = Object.create(null);
-  function addEdge(from, to) {
-    if (graph[from] == null) {
-      graph[from] = [to];
-    } else {
-      graph[from].push(to);
-    }
-  }
-
-  for (let [from, to] of edges.map(r => r.split('-'))) {
-    addEdge(from, to);
-    addEdge(to, from);
-  }
-  return graph;
-}
-
-exports.roadGraph = buildGraph(roads);
+exports.roadGraph = buildGraph(roads.map(edge => edge.split('-')));
 
 class VillageState {
   constructor(place, parcels) {
