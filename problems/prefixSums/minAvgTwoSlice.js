9918171d9d5a86fb3b65ef30cffc3287d46f2da3
TahmidRahman <tahmidrahman@USER-no-MacBook-Pro.local>
2020-03-23 22:15:11 +0600

    Update minAvgTwoSlice to use prefix sums

--- problems/prefixSums/minAvgTwoSlice.js
+++ problems/prefixSums/minAvgTwoSlice.js
@@ -1,23 +1,35 @@
+function prefixSum(A) {
+  const prefixSumResult = [0];
+  A.forEach((item, index) => {
+    prefixSumResult.push(item + prefixSumResult[index]);
+  });
+
+  return prefixSumResult;
+}
+
 function solution(A) {
-  const { length: n } = A;
+  // write your code in JavaScript (Node.js 8.9.4)
   let minAvg = Infinity;
-  let foundIndex = -1;
-  for (let i = 0; i < n - 1; i++) {
-    const avg = (A[i] + A[i + 1]) / 2;
+  let startingIndex = 0;
+  const { length: n } = A;
+  const sums = prefixSum(A);
+  for (let i = 2; i <= n; i++) {
+    const avg = (sums[i] - sums[i - 2]) / 2;
     if (avg < minAvg) {
       minAvg = avg;
-      foundIndex = i;
-    }
-  }
-  for (let i = 0; i < n - 2; i++) {
-    const avg = (A[i] + A[i + 1] + A[i + 2]) / 3;
-    if (avg < minAvg) {
-      minAvg = avg;
-      foundIndex = i;
+      startingIndex = i - 2;
     }
   }
 
-  return foundIndex;
+  for (let i = 3; i <= n; i++) {
+    const avg = (sums[i] - sums[i - 3]) / 3;
+    if (avg < minAvg) {
+      minAvg = avg;
+      startingIndex = i - 3;
+    }
+  }
+
+  return startingIndex;
 }
 
 console.log(solution([4, 2, 2, 5, 1, 5, 8])); // Should return 1
