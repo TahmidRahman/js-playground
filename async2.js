9d839483c9c398e6e266b5be674d5ff83aecf47d
TahmidRahman <tahmid131@gmail.com>
2020-08-03 02:21:10 +0600

    Improve code

--- async2.js
+++ async2.js
@@ -1,12 +1,39 @@
 // Continuation of async.js
-const { banyan, bigOak, request, pingAllNeighbors } = require('./crow-tech');
+const {
+  banyan,
+  bigOak,
+  apple,
+  request,
+  routeRequest,
+  pingAllNeighbors,
+} = require('./crow-tech');
 
+// Simple request response
 request(banyan, 'bigOak', 'note', 'Cow if any enemy is in the territory')
   .then((response) =>
     console.log(`Note delivered with response ${JSON.stringify(response)}`)
   )
   .catch((error) => console.log(error.message));
 
+// Ping available nests
 pingAllNeighbors(bigOak).then((neighbors) =>
   console.log(`Available neighbors of ${bigOak.name} are ${neighbors}`)
 );
+
+// Flood crow network
+request(
+  apple,
+  'banyan',
+  'gossip',
+  'There is a crow wedding 2 kms away of the network'
+);
+
+// Not direct neighbor
+// Simple request response
+routeRequest(bigOak, 'apple', 'note', 'Cow if any enemy is in the territory')
+  .then((response) =>
+    console.log(
+      `Note routed and delivered with response ${JSON.stringify(response)}`
+    )
+  )
+  .catch((error) => console.log(error.message));
--- crow-tech.js
+++ crow-tech.js
@@ -9,12 +9,33 @@ function readStorage(name, callback) {
   }
 }
 
+const crowNetwork = ['bigOak', 'banyan', 'yarn', 'sycamore', 'willow', 'apple'];
+
+const crowNeighborGraph = {
+  bigOak: ['banyan', 'willow', 'yarn'],
+  banyan: ['bigOak', 'apple'],
+  yarn: ['sycamore', 'apple', 'bigOak'],
+  sycamore: ['yarn', 'willow'],
+  willow: ['bigOak', 'sycamore'],
+  apple: ['banyan', 'yarn'],
+};
+
+function Nest(name) {
+  this.name = name;
+
+  this.neighbors = crowNeighborGraph[name];
+  this.state = {};
+}
+
 function defineRequestType(type, handler) {
-  this.handlers[type] = handler;
+  if (!Nest.prototype.handlers) {
+    Nest.prototype.handlers = {};
+  }
+  Nest.prototype.handlers[type] = handler;
 }
 
 function requestType(type, handler) {
-  defineRequestType.call(this, type, (nest, content, source, callback) => {
+  defineRequestType(type, (nest, content, source, callback) => {
     try {
       Promise.resolve(handler(nest, content, source)).then(
         (value) => callback(null, value),
@@ -25,54 +46,48 @@ function requestType(type, handler) {
     }
   });
 }
-const crowNetwork = ['bigOak', 'banyan', 'yarn', 'riverSide'];
 
-function Nest(name) {
-  this.name = name;
-  this.handlers = Object.create(null);
-  this.readStorage = readStorage;
-  this.neighbors = crowNetwork.filter((nestName) => nestName != name);
+// install note handler
+requestType('note', (nest, content, source, done) => {
+  console.log(`${nest.name} received note "${content}" from ${source}`);
+  return { received: true };
+});
 
-  // install note handler
-  this._requestType('note', (nest, content, source, done) => {
-    console.log(`${nest.name} received note "${content}" from ${source}`);
-    return { crowStatus: 200 };
-  });
+// install ping handler
+requestType('ping', () => 'pong');
 
-  // install ping handler
-  this._requestType('ping', () => 'pong');
+const allNests = crowNetwork.map((name) => new Nest(name));
 
-  // unknown type handler
-  this._requestType('unkwown', (_, _2, _3, done) => {
-    setTimeout(done, CROW_NETWORK_LATENCY);
-  });
+function everywhere(handler) {
+  allNests.forEach(handler);
 }
-
-Nest.prototype._requestType = requestType;
 Nest.prototype.send = function (dest, type, content, callback) {
-  crowFetch(this.name, dest, type, content, callback)
+  const findAndHandleRequest = new Promise((resolve, reject) => {
+    // Because each nest has a scope of its neighboring nests
+    const neighborNests = allNests.filter(({ name }) =>
+      this.neighbors.includes(name)
+    );
+    const destinationNest = neighborNests.find(({ name }) => name == dest);
+    if (!destinationNest) {
+      reject(new Error(`${dest} not found in the neighbors!`));
+    } else {
+      setTimeout(() => {
+        resolve(
+          destinationNest.handlers[type](
+            destinationNest,
+            content,
+            this.name,
+            callback
+          )
+        );
+      }, CROW_NETWORK_LATENCY); // To understand retry policy
+    }
+  });
+
+  findAndHandleRequest
     .then((value) => callback(null, value))
     .catch((failed) => callback(failed));
 };
-
-const nests = crowNetwork.map((name) => new Nest(name));
-
-function crowFetch(source, dest, type, content, callback) {
-  const target = nests.find((nest) => nest.name == dest);
-  if (!target) {
-    return Promise.reject(new Error(`${dest} not found in crow network`));
-  } else {
-    return new Promise((resolve, reject) => {
-      if (target.handlers[type]) {
-        setTimeout(() => {
-          resolve(target.handlers[type](target, content, source, callback));
-        }, CROW_NETWORK_LATENCY);
-      } else {
-        reject(new Error('Handler not found'));
-      }
-    });
-  }
-}
 
 class TimeOut extends Error {}
 
@@ -102,13 +117,13 @@ function request(nest, target, type, content) {
   });
 }
 
-const [bigOak, banyan] = nests;
+const [bigOak, banyan, yarn, sycamore, willow, apple] = allNests;
 
 exports.bigOak = bigOak;
 exports.banyan = banyan;
+exports.apple = apple;
+
 exports.request = request;
-// make riverSide unavailable for ping
-nests[3].handlers.ping = undefined;
 
 exports.pingAllNeighbors = function (nest) {
   const requests = nest.neighbors.map((neighbor) => {
@@ -121,3 +136,78 @@ exports.pingAllNeighbors = function (nest) {
     return nest.neighbors.filter((_, i) => result[i]);
   });
 };
+
+everywhere((nest) => {
+  nest.state.gossip = [];
+});
+
+function sendGossip(nest, message, exceptFor = null) {
+  nest.state.gossip.push(message);
+  for (let neighbor of nest.neighbors) {
+    if (neighbor == exceptFor) continue;
+    request(nest, neighbor, 'gossip', message);
+  }
+}
+
+requestType('gossip', (nest, message, source) => {
+  if (nest.state.gossip.includes(message)) return;
+  console.log(`${nest.name} received gossip ${message} from ${source}`);
+  sendGossip(nest, message, source);
+});
+
+requestType('connections', (nest, { name, neighbors }, source) => {
+  let connections = nest.state.connections;
+  if (JSON.stringify(connections.get(name)) == JSON.stringify(neighbors))
+    return;
+  connections.set(name, neighbors);
+  broadcastConnections(nest, name, source);
+});
+
+function broadcastConnections(nest, name, exceptFor = null) {
+  for (let neighbor of nest.neighbors) {
+    if (neighbor == exceptFor) continue;
+    request(nest, neighbor, 'connections', {
+      name,
+      neighbors: nest.state.connections.get(name),
+    });
+  }
+}
+
+everywhere((nest) => {
+  nest.state.connections = new Map();
+  nest.state.connections.set(nest.name, nest.neighbors);
+  broadcastConnections(nest, nest.name);
+});
+
+function findRoute(from, to, connections) {
+  let work = [{ at: from, via: null }];
+  for (let i = 0; i < work.length; i++) {
+    let { at, via } = work[i];
+    for (let next of connections.get(at) || []) {
+      if (next == to) return via;
+      if (!work.some((w) => w.at == next)) {
+        work.push({ at: next, via: via || next });
+      }
+    }
+  }
+
+  return null;
+}
+
+function routeRequest(nest, target, type, content) {
+  if (nest.neighbors.includes(target)) {
+    return request(nest, target, type, content);
+  } else {
+    let via = findRoute(nest.name, target, nest.state.connections);
+    if (!via) {
+      throw new Error(`No route to ${target}`);
+    }
+    return request(nest, via, 'route', { target, type, content });
+  }
+}
+
+requestType('route', (nest, { target, type, content }) =>
+  routeRequest(nest, target, type, content)
+);
+
+exports.routeRequest = routeRequest;
