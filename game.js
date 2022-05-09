9d710c1601fb26153e1728a3d43372bea7ea6bac
TahmidRahman <tahmid131@gmail.com>
2022-05-10 01:57:37 +0600

    Add drawActors method1

--- game.js
+++ game.js
@@ -179,3 +179,18 @@ function drawGrid(level) {
     )
   );
 }
+
+function drawActors(actors) {
+  return elt(
+    'div',
+    {},
+    ...actors.map((actor) => {
+      let rect = elt('div', { class: `actor ${actor.type}` });
+      rect.style.width = `${actor.size.x * scale}px`;
+      rect.style.height = `${actor.size.y * scale}px`;
+      rect.style.left = `${actor.pos.x * scale}px`;
+      rect.style.top = `${actor.pos.y * scale}px`;
+      return rect;
+    })
+  );
+}
