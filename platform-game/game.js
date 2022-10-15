2822e7ea9361686f215fde93120cb258594c207f
TahmidRahman <tahmid131@gmail.com>
2022-10-16 02:43:50 +0600

    Add motion & collision in platform game

--- platform-game/game.js
+++ platform-game/game.js
@@ -31,6 +31,22 @@ class Level {
   }
 }
 
+Level.prototype.touches = function (pos, size, type) {
+  var xStart = Math.floor(pos.x);
+  var xEnd = Math.ceil(pos.x + size.x);
+  var yStart = Math.floor(pos.y);
+  var yEnd = Math.ceil(pos.y + size.y);
+
+  for (var y = yStart; y < yEnd; y++) {
+    for (var x = xStart; x < xEnd; x++) {
+      let isOutside = x < 0 || x >= this.width || y < 0 || y >= this.height;
+      let here = isOutside ? 'wall' : this.rows[y][x];
+      if (here == type) return true;
+    }
+  }
+  return false;
+};
+
 class State {
   constructor(level, actors, status) {
     this.level = level;
@@ -47,6 +63,29 @@ class State {
   }
 }
 
+State.prototype.update = function (time, keys) {
+  let actors = this.actors.map((actor) => actor.update(time, this, keys));
+  let newState = new State(this.level, actors, this.status);
+  if (newState.status != 'playing') return newState;
+  let player = newState.player;
+  if (newState.level.touches(player.pos, player.size, 'lava'))
+    return new State(this.level, actors, 'lost');
+  for (let actor of actors) {
+    if (actor != player && overlap(actor, player)) {
+      newState = actor.collide(newState);
+    }
+  }
+  return newState;
+};
+
+function overlap(actor1, actor2) {
+  return (
+    actor1.pos.x + actor1.size.x > actor2.pos.x &&
+    actor1.pos.x < actor2.pos.x + actor2.size.x &&
+    actor1.pos.y + actor1.size.y > actor2.pos.y &&
+    actor1.pos.y < actor2.pos.y + actor2.size.y
+  );
+}
 class Vec {
   constructor(x, y) {
     this.x = x;
@@ -106,6 +145,9 @@ class Lava {
 }
 
 Lava.prototype.size = new Vec(1, 1);
+Lava.prototype.collide = function (state) {
+  return new State(state.level, state.actors, 'lost');
+};
 
 class Coin {
   constructor(pos, basePos, wobble) {
@@ -125,6 +167,12 @@ class Coin {
 }
 
 Coin.prototype.size = new Vec(0.6, 0.6);
+Coin.prototype.collide = function (state) {
+  let filtered = state.actors.filter((actor) => actor != this);
+  let status = state.status;
+  if (!filtered.some((a) => a.type == 'coin')) status = 'won';
+  return new State(state.level, filtered, status);
+};
 
 const levelChars = {
   '.': 'empty',
